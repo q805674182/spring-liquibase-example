4b2b6d49f274b333b0766f526ef8857a77c7e422
Devin Shoemaker <devinshoe@gmail.com>
2017-05-06 09:46:31 -0500

    Hello World API This API returns "Hello World" from the end point /.

---
+++ src/main/java/com/devinshoemaker/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.devinshoemaker;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * API that returns "Hello World".
+ *
+ * @author Devin Shoemaker (devinshoe@gmail.com)
+ */
+@RestController
+public class HelloWorldController {
+
+    /**
+     * End point to return "Hello World".
+     *
+     * @return "Hello World" string.
+     */
+    @RequestMapping("/")
+    public String getHelloWorld() {
+        return "Hello World";
+    }
+
+}
+
